6aa48aa2cc198f1e5e172c2ceab9d17c907dfbe0
Attila Török <ansansans8@gmail.com>
2024-06-15 16:13:43 +0200

    Init

---
+++ src/test/java/dev/fitlife/dietservice/DietServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fitlife.dietservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
